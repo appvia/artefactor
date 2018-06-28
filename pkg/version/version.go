f51fd019cb748f25ef2962b3c09acebc00aed894
Lewis Marshall <lewis.marshall@digital.homeoffice.gov.uk>
2018-06-28 09:56:51 +0100

    Add Build - Makefile - CI build - Readme updates

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,23 +1,20 @@
 package version
 
 var (
-	gitVersion   string
-	gitCommit    string
-	gitTreeState string
+	gitSha     string
+	gitVersion string
 )
 
 // Version represents version data.
 type Version struct {
-	Version      string
-	Commit       string
-	GitTreeState string
+	Version string
+	Sha     string
 }
 
 // Get returns the overall codebase version.
 func Get() Version {
 	return Version{
-		Version:      gitVersion,
-		Commit:       gitCommit,
-		GitTreeState: gitTreeState,
+		Version: gitVersion,
+		Sha:     gitSha,
 	}
 }
